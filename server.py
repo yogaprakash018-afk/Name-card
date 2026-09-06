7ec491e2610607627b99f93e267c9d2ae917e75a
Yogaprakash <yogaprakash018@gmail.com>
2026-09-06 14:16:59 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", methods = ["GET"], endpoint = "homepage")
+def home():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(port = 3000, debug = True)
